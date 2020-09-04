fc5b9f223aa2820e39b39d7927b07a4fcc67e06a
renancosta.dsn.shop <renancosta.dsn.shop@alterdata.com.br>
2020-09-04 19:23:07 -0300

    ajustando url

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://api-grade.herokuapp.com/',
+  baseURL: 'https://api-grade.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
